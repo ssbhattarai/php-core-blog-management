31b319e9c9146eb9351baffa3b42e5c11fa730b1
ssbzeref <nascentsurya@gmail.com>
2020-08-05 16:48:02 +0545

    dashboard design

---
+++ dashboard.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/adminstyles.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>ADMIN</title>
+    <style>
+    </style>
+</head>
+<body>
+   <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-2">
+                <h1>shyam</h1>
+                <ul id="side_menu" class="nav flex-column nav-pills">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="dashboard.php">
+                        <i class="fa fa-tachometer" aria-hidden="true"></i>
+                        &nbsp;Dashboard
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                        <i class="fa fa-list" aria-hidden="true"></i>
+                        &nbsp;Add New post
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                        <i class="fa fa-tags" aria-hidden="true"></i>
+                        &nbsp;Categories
+                    </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                        <i class="fa fa-users" aria-hidden="true"></i>
+                        &nbsp;Manage Admins
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                        <i class="fa fa-comments" aria-hidden="true"></i>
+                        &nbsp;Comments
+                        </a>
+                    </li><li class="nav-item">
+                        <a class="nav-link" href="#">
+                        <i class="fa fa-rss" aria-hidden="true"></i>
+                        &nbsp;Live Blog
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                        <i class="fa fa-sign-out" aria-hidden="true"></i>
+                        &nbsp;Logout
+                        </a>
+                    </li>
+                    </ul>
+            </div> <!-- ending of side area-->
+                <div class="col-sm-10">
+                    <h1>Admin Dashboard </h1>
+                    <p>
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of thethis is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of thethis is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of thethis is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of thethis is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of thethis is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of thethis is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of thethis is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of thethis is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of thethis is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of thethis is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of thethis is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of thethis is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of thethis is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of thethis is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of thethis is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of thethis is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of thethis is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of thethis is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of thethis is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of thethis is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of thethis is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of the
+                        this is dummy test for test of therm 
+                    </p>
+            </div> <!-- ending of side area-->
+        </div> <!-- ending  of row-->
+   </div> <!-- ennd containger-->
+   <div id="footer"> <!-- Footer div -->
+        <hr><p> | &copy; | 2020 <a href="https://sambhattarai.com.np" target="_blank">SAMBHATTARAI</a> </p>
+   </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <title>INDEX PAGE</title>
+</head>
+<body>
+    <h1>HELLO WORLD</h1>
+    <?php echo "this is php echo"; ?>
+</body>
+</html>
