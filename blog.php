01b0dabe2357ff047b04d48fc024da515c63a0c5
ssbzeref <nascentsurya@gmail.com>
2020-08-08 09:51:09 +0545

    Added the navbar and links for frontend

--- blog.php
+++ blog.php
@@ -21,10 +21,10 @@
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
+      <li class="nav-item">
         <a class="nav-link" href="index.php">Home</a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item active">
         <a class="nav-link" href="blog.php">Blog</a>
       </li>
       <li class="nav-item dropdown">
