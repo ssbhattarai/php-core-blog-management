46f1444737bd0f4f46c2ef7a229781cd2b8cc3fd
ssbzeref <nascentsurya@gmail.com>
2020-08-08 10:02:08 +0545

    responsive navbar

--- blog.php
+++ blog.php
@@ -7,39 +7,30 @@
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <script src="js/bootstrap.min.js"></script>
     <link rel="stylesheet" type="text/css" href="css/public.css">
-    <title>INDEX PAGE</title>
+    <title>Blog</title>
 </head>
 <body>
     <div style="height:10px;background: #227b5a;"></div>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-        <div class="nav-header">
+<div class="container">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <div class="nav-header">
             <a href="index.php" style="argin: -6px;margin-right: 10px;">
                 <img src="static/sundarBlog.png" alt="sundarblog" style="width:8em;">
             </a>
-        </div>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
+    </div>
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
       <li class="nav-item">
         <a class="nav-link" href="index.php">Home</a>
       </li>
       <li class="nav-item active">
         <a class="nav-link" href="blog.php">Blog</a>
       </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
       <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
       </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
@@ -48,7 +39,9 @@
     </form>
   </div>
   </div>
-</nav>  
+</nav>
+<div style="height:10px;background: #227b5a;"></div>
+
 
 
 
