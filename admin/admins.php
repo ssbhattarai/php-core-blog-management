01bc1c443185da6c116197e550b6f7f43772dabd
ssbzeref <nascentsurya@gmail.com>
2020-08-15 16:25:33 +0545

    Admin Data fetched

--- admin/admins.php
+++ admin/admins.php
@@ -152,145 +152,38 @@
 
 
                 <!-- Category Table -->
-                <h4 style="margin-top: 20px;text-align:center; font-weight:bold;" > Category Table </h4>
+                <h4 style="margin-top: 20px;text-align:center; font-weight:bold;" > Admins Table </h4>
                 <div style="margin-top: 26px;" class="table-responsive">
                     
                     <?php 
-                        // Pagination
-                        if (isset($_GET['page_no']) && $_GET['page_no']!="") {
-                            $page_no = $_GET['page_no'];
-                        } else {
-                                $page_no = 1;
-                        }
-                        $total_records_per_page = 3;
-                        
-                        $offset = ($page_no-1) * $total_records_per_page;
-                        $previous_page = $page_no - 1;
-                        $next_page = $page_no + 1;
-                        $adjacents = "2";
+                        $adminData = '';
+                        global $connection;
+                        $admin = mysqli_query($connection, "SELECT * FROM admins order by datetime desc");
+                        // while($allData  = mysqli_fetch_assoc($admin)){
 
-                        $result_count = mysqli_query(
-                            $connection,
-                            "SELECT COUNT(*) AS total_categories FROM `category`"
-                        );
-                        $total_records = mysqli_fetch_array($result_count);
-                        $total_records = $total_records['total_categories'];
-                        $total_number_of_pages = ceil($total_records / $total_records_per_page);
-                        $second_last = $total_number_of_pages - 1;
-
-
-                        $dataErr = "";
-                        $sql = "SELECT * FROM category LIMIT $offset, $total_records_per_page";
-                        // $sql = "SELECT * FROM category";
-                        $res_data = $connection->query($sql);
-                        // echo $result;
-
+                        // }
                        
-                    ?>
-             <?php if(!$dataErr) { ?>
+                    ?> 
                     <table class="table table-striped">
                         <thead class="thead-dark">
                             <tr>
                                 <th scope="col">Id</th>
-                                <th scope="col">Category </th>
-                                <th scope="col">Created Date</th>
-                                <th scope="col">Creator Name</th>
+                                <th scope="col">Datetime </th>
+                                <th scope="col">Username</th>
+                                <th scope="col">Added by</th>
                             </tr>
                         </thead>
                         <tbody>
                         <?php 
-                        if($res_data->num_rows > 0) {
-                            while($row = $res_data->fetch_assoc()) {
-                                echo "<tr><td>{$row['id']}</td><td>{$row['category_name']}</td><td>{$row['datetime']}</td><td>{$row['creatorname']}</td></tr>\n";
+                        if($admin->num_rows > 0) {
+                            while($row = mysqli_fetch_assoc($admin)) {
+                                echo "<tr><td>{$row['id']}</td><td>{$row['datetime']}</td><td>{$row['username']}</td><td>{$row['addedby']}</td></tr>\n";
                             }
                         } else {
                             $dataErr = "No data To display";
                         }
                         ?>
-                    <?php if($dataErr) {?>
-                    <div class="alert alert-info" role="alert">
-                         <?php echo $dataErr; ?>
-                    </div>
-                    <?php } ?>
                     </table>
-                    <div style='padding: 10px 20px 0px; border-top: dotted 1px #CCC;'>
-                        <strong>Page <?php echo $page_no." of ".$total_number_of_pages; ?></strong>
-                    </div>
-                    <nav aria-label="Page navigation example">
-                    <ul class="pagination justify-content-end">
-                        <?php if($page_no > 1){
-                        echo "<li><a class='page-link' href='?page_no=1'>First Page</a></li>";
-                        } ?>
-                            
-                        <li class="page-item" <?php if($page_no <= 1){ echo "class='disabled'"; } ?>>
-                        <a class='page-link' <?php if($page_no > 1){
-                        echo "href='?page_no=$previous_page'";
-                        } ?>>Previous</a>
-                        </li>
-                        <?php     
-                        if ($total_number_of_pages <= 10){  	 
-                            for ($counter = 1; $counter <= $total_number_of_pages; $counter++){
-                            if ($counter == $page_no) {
-                            echo "<li class='active'><a class='page-link' >$counter</a></li>";	
-                                    }else{
-                                echo "<li><a class='page-link' href='?page_no=$counter'>$counter</a></li>";
-                                        }
-                                }
-                        }elseif ($total_number_of_pages > 10){
-                            if($page_no <= 4) { 
-                                for ($counter = 1; $counter < 8; $counter++){ 
-                                if ($counter == $page_no) {
-                                   echo "<li class='active'><a class='page-link'>$counter</a></li>"; 
-                                }else{
-                                          echo "<li class=\"page-item\"><a class='page-link' href='?page_no=$counter'>$counter</a></li>";
-                                               }
-                               }
-                               echo "<li class=\"page-item\"><a class='page-link'>...</a></li>";
-                               echo "<li class=\"page-item\"><a class='page-link' href='?page_no=$second_last'>$second_last</a></li>";
-                               echo "<li class=\"page-item\"><a class='page-link' href='?page_no=$total_number_of_pages'>$total_number_of_pages</a></li>";
-                               }
-                            }
-                            // elseif($page_no > 4 && $page_no < $total_number_of_pages - 4) { 
-                            //     echo "<li class='page-item'><a class='page-link' href='?page_no=1'>1</a></li>";
-                            //     echo "<li class='page-item'><a class='page-link' href='?page_no=2'>2</a></li>";
-                            //     echo "<li class='page-item'><a class='page-link'>...</a></li>";
-                            //     for (
-                            //          $counter = $page_no - $adjacents;
-                            //          $counter <= $page_no + $adjacents;
-                            //          $counter++
-                            //          ) { 
-                            //          if ($counter == $page_no) {
-                            //      echo "<li class='page-item' class='active'><a class='page-link'>$counter</a></li>"; 
-                            //      }else{
-                            //             echo "<li class='page-item'><a class='page-link' href='?page_no=$counter'>$counter</a></li>";
-                            //               }                  
-                            //            }
-                            //     echo "<li class='page-item'><a class='page-link'>...</a></li>";
-                            //     echo "<li class='page-item'><a class='page-link' href='?page_no=$second_last'>$second_last</a></li>";
-                            //     echo "<li class='page-item'><a class='page-link' href='?page_no=$total_number_of_pages'>$total_number_of_pages</a></li>";
-                        //  } else {
-                        //     echo "<li><a href='?page_no=1'>1</a></li>";
-                        //     echo "<li><a href='?page_no=2'>2</a></li>";
-                        //     echo "<li><a>...</a></li>";
-                        //     for (
-                        //          $counter = $total_no_of_pages - 1;
-                        //          $counter <= $total_no_of_pages;
-                        //          $counter++
-                        //          ) {
-                        //          if ($counter == $page_no) {
-                        //      echo "<li class='active'><a>$counter</a></li>"; 
-                        //      }else{
-                        //             echo "<li><a href='?page_no=$counter'>$counter</a></li>";
-                        //      }                   
-                        //          }
-                        //     }
-                                
-                        ?>
-                        <?php if($page_no < $total_number_of_pages){
-                        echo "<li class='page-item'><a class='page-link' href='?page_no=$total_number_of_pages'>Last &rsaquo;&rsaquo;</a></li>";
-                        } ?>
-                        </ul>
-        <?php } ?>
                 </div>
             </div> <!-- ending of side area-->
 
