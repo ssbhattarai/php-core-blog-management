7e95d9d8adb255ae97cb9bc7fed6e3ab73e693ba
ssbzeref <nascentsurya@gmail.com>
2020-08-16 19:52:11 +0545

    login with makeing a functions

--- include/functions.php
+++ include/functions.php
@@ -1,6 +1,15 @@
+<?php include_once("./database.php") ?>
 <?php 
 
 function redirect($location){
     header("Location: $location");
     exit;
+}
+
+
+function login_attempt($username, $password){
+    global $connection;
+    $query = "SELECT * FROM admins WHERE username='$username' and password = '$password'"
+    // $execute = mysqli_query($connection, $query);
+
 }
